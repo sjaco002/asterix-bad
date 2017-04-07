dd3a0a6d636ccaf3ff0571f4ca86cc1f01f67ae5
Steven Glenn Jacobs <sjaco002@ucr.edu>
2017-04-07 15:56:41 -0700

    Changed to runtime context variable

--- asterix-bad/src/main/java/org/apache/asterix/bad/lang/statement/CreateProcedureStatement.java
+++ asterix-bad/src/main/java/org/apache/asterix/bad/lang/statement/CreateProcedureStatement.java
@@ -45,8 +45,8 @@ import org.apache.asterix.lang.common.base.Statement;
 import org.apache.asterix.lang.common.clause.LetClause;
 import org.apache.asterix.lang.common.expression.CallExpr;
 import org.apache.asterix.lang.common.expression.LiteralExpr;
+import org.apache.asterix.lang.common.expression.RuntimeContextVarExpr;
 import org.apache.asterix.lang.common.expression.VariableExpr;
-import org.apache.asterix.lang.common.literal.IntegerLiteral;
 import org.apache.asterix.lang.common.literal.StringLiteral;
 import org.apache.asterix.lang.common.statement.DeleteStatement;
 import org.apache.asterix.lang.common.statement.Query;
@@ -193,7 +193,8 @@ public class CreateProcedureStatement implements IExtensionStatement {
             DeleteStatement delete = (DeleteStatement) getProcedureBodyStatement();
             FLWOGRExpression flwogr = (FLWOGRExpression)delete.getQuery().getBody();
             for (VariableExpr var : varList){
-                Expression con = new LiteralExpr(new IntegerLiteral(var.getVar().getId()));
+                //Expression con = new LiteralExpr(new IntegerLiteral(var.getVar().getId()));
+                Expression con = new RuntimeContextVarExpr(var.getVar().getValue());
                 LetClause let = new LetClause(var, con);
                 flwogr.getClauseList().add(0, let);
             }
